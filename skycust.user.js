e2260a3ab9e1e275f35bb95588ae2a7acfa4335d
Sohum Banerjea <sohumb@gmail.com>
2009-12-12 08:47:45 +1100

    Initial scaffolding (that is, actual code)

---
+++ skycust.user.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @name             Skycust
+// @description      A forum avatar-replacer based on a remote canonical image source.
+// @include          http://skyrates.net/forum/*
+// ==/UserScript==
