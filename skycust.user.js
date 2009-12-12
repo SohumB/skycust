7a154befbb9d0078d78645a1307809e8850fa81c
Sohum Banerjea <sohumb@gmail.com>
2009-12-13 01:03:43 +1100

    This should work. I haven't tested it extensively yet, and I don't particularly like that the browser requests the final image twice...

--- skycust.user.js
+++ skycust.user.js
@@ -6,23 +6,37 @@
 // @require          http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
-var avatars = {
-  'Calvin':    'http://skyrates.net/resources/avatar/circle7.png',
-  'Sadistica': 'http://skyrates.jusque.net/betsy.png'
+var servers = [ "http://skyrates.jusque.net/skycust/",
+		"http://skyrates.jusque.net/skyrates/" ];
+
+var local = { 'Sadistica': 'http://skyrates.jusque.net/betsy.png' };
+
+function set_avatar_link(span, link) {
+  $(span).find('span.postdetails img').attr({
+    src:    link,
+    width:  80,
+    height: 90
+  });
 }
 
-function find_avatar(name) {
-  return avatars[name];
+function check_asynchronously(span, name, which_server) {
+  if (servers[which_server]) {
+    var link = servers[which_server] + name;
+    $.ajax({ url : link,
+	     type: "GET",
+	     success: function (response) { set_avatar_link(span, link); },
+	     error: function (req,stat,err) { check_asynchronously(span, name, which_server+1); }});
+  }
+}
+
+function find_and_set_avatar(span, name) {
+  var link = local[name];
+  if (link) { set_avatar_link(span,link); return; }
+
+  check_asynchronously(span, name, 0);
 }
 
 $('td:has(span.name):has(span.postdetails)').each(function () {
   var name = $(this).find('span.name').text();
-  var avatar = find_avatar(name);
-  if (avatar) {
-    $(this).find('span.postdetails img').attr({
-      src:    avatar,
-      width:  80,
-      height: 90
-    });
-  }
+  find_and_set_avatar(this, name);
 });
