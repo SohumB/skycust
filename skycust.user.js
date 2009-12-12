300f88f6a65b1e5678d0d1019a9b3005c908ad43
Kathryn <yowsah@gmail.com>
2009-12-12 14:38:17 +1100

    We are all individuals.

--- skycust.user.js
+++ skycust.user.js
@@ -2,4 +2,9 @@
 // @name             Skycust
 // @description      A forum avatar-replacer based on a remote canonical image source.
 // @include          http://skyrates.net/forum/*
+// @require          http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
+
+$('img[src^="images/avatars/gallery/skyrates/circle"]').attr({
+  src: 'http://skyrates.jusque.net/betsy.png'
+});
