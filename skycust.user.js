a7d06d4aed1d4f88c22195b6f2d251cf2eddc4a4
Kathryn <yowsah@gmail.com>
2009-12-12 18:42:21 +1100

    Some of us are more different than others.

--- skycust.user.js
+++ skycust.user.js
@@ -5,6 +5,19 @@
 // @require          http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
-$('img[src^="images/avatars/gallery/skyrates/circle"]').attr({
-  src: 'http://skyrates.jusque.net/betsy.png'
+var avatars = {
+  'Calvin':    'http://skyrates.net/resources/avatar/circle7.png',
+  'Sadistica': 'http://skyrates.jusque.net/betsy.png'
+}
+
+$('td:has(span.name):has(span.postdetails)').each(function () {
+  var name = $(this).find('span.name').text();
+  var avatar = avatars[name];
+  if (avatar) {
+    $(this).find('span.postdetails img').attr({
+      src:    avatar,
+      width:  80,
+      height: 90
+    });
+  }
 });
